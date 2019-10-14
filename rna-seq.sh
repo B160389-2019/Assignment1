0cff74de269185b0a26b8c41ac8b7ee1af88077e
B160389-2019 <s1914230@ed.ac.uk>
2019-10-14 01:49:43 +0100

    Third version of rna-seq.sh shell script

--- rna-seq.sh
+++ rna-seq.sh
@@ -5,3 +5,10 @@ mkdir -p ~/output/QC
 mkdir -p ~/output/bam
 mkdir -p ~/output/result
 
+#Copy file from Assignment1
+cp /localdisk/data/BPSM/Assignment1/Tbb_genome/Tb927_genome.fasta.gz /localdisk/home/s1914230
+  
+#Unzip file
+gunzip Tb927_genome.fasta.gz 
+#Buliding index for bowtie2
+bowtie2-build Tb927_genome.fasta index 
